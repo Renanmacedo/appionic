8a05cf187b2dff4efa7490a32884462f91b132ea
Renan Aquino <renan.aquinolima@gmail.com>
2017-05-10 01:30:13 -0300

    novas funcionalidades implementadas

--- piloto2/www/js/listaCtrl.js
+++ piloto2/www/js/listaCtrl.js
@@ -1,17 +1,34 @@
 angular.module('starter')
     .controller('listaCtrl',function($scope,$ionicSideMenuDelegate){
         $scope.listaCarros = [{
-          modelo: "BMW - Sport",
-          codigo: 199  
+          "modelo": "BMW - Sport",
+          "codigo": 199,
+          "valor": 33934  
         },{
-          modelo: "FERRARI - Sport",
-          codigo: 344
+         "modelo": "FERRARI - Sport",
+          "codigo": 199,
+          "valor": 102303
         },{
-            modelo: "POSH - XSAUTO",
-            codigo: 999
+            "modelo": "POSH - XSAUTO",
+            "codigo": 199,
+            "valor": 339423
         }
         ];
         $scope.showHide = function(){
             $ionicSideMenuDelegate.toggleLeft();
         };
     });
+angular.module('starter')
+    .controller('carroEscolhidoCtrl', function($scope, $stateParams){
+        
+    $scope.carroEscolhido = angular.fromJson($stateParams.carro);
+    
+    $scope.listaAcessorios = [{"nome":"Airbeg", "preco":900}, {"nome": "Freio ABS", "preco":800}];
+    $scope.acessorioMarcado = function(acessorio, isMarcado){
+        if(isMarcado){
+            $scope.carroEscolhido.valor += acessorio.preco;
+        }else{
+            $scope.carroEscolhido.valor -=  acessorio.preco;
+        }
+    }
+ });
--- piloto2/www/js/routes.js
+++ piloto2/www/js/routes.js
@@ -1,9 +1,15 @@
 angular.module("starter")
  .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise("listagem");
-    $stateProvider.state('listagem',{
+    $stateProvider
+    .state('listagem',{
         url:"/listagem",
         templateUrl: "templates/listagem.html",
         controller: "listaCtrl" 
+    })
+    .state('carroescolhido',{
+        url: '/carroescolhido/:carro',
+        templateUrl: 'templates/carroescolhido.html',
+        controller: 'carroEscolhidoCtrl'
     });
  });
